9ca626e410c638409e28415f58de7b448ab55f8a
alhinator <alex@leghart.org>
2024-12-03 16:58:13 -0800

    updated PWA config + built

---
+++ dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cmpm-121-final/sw.js', { scope: '/cmpm-121-final/' })})}
--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,30 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
     // config options
     base: "/cmpm-121-final/",
-    build:{
-        outDir:"./dist"
+    build: {
+        outDir: "./dist"
     },
     plugins: [
-        VitePWA({ registerType: 'autoUpdate' })
-      ]
+        VitePWA({
+            registerType: 'autoUpdate', includeAssets: ['./icons/128.png', './icons/512.png'],
+            manifest: {
+                name: '121 Farming Game',
+                short_name: '121Farm',
+                description: 'cmpm 121 final project',
+                theme_color: '#094e67',
+                icons: [
+                    {
+                        src: './icons/192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: './icons/512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ]
 });
